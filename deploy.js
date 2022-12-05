429baee1c8a6c3d4b3883a95c90949da760770e2
CodexpathCommunity <codexpath3@gmail.com>
2022-12-05 14:06:33 +0100

    add private key

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
-require(dotenv).config()
+require("dotenv").config()
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
@@ -8,6 +8,12 @@ async function main() {
     process.env.PRIVATE_KEY,
     provider
   );
+//   const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+//     let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+//       encryptedJson,
+//       process.env.PRIVATE_KEY_PASSWORD
+//     );
+//     wallet = wallet.connect(provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
--- encryptKey.js
+++ encryptKey.js
@@ -1,14 +1,14 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
-require(dotenv).config();
+require("dotenv").config();
 
-function main() {
+async function main() {
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
-  const encryptedJsonKey = wallet.encrypt(
-    process.env.PRIVATE_KEY_PASSWORD,
-    process.env.PRIVATE_KEY
-  );
-
+   const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD,
+        process.env.PRIVATE_KEY
+    )
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
 }
 
 main()
